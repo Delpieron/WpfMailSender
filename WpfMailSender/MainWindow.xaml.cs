182bdf319c5f2b4915bf75d555f8ce3960a211f2
Krzysztof Wróblewski <k.wroblewski@microsoft.wsei.edu.pl>
2019-10-07 19:36:20 +0200

    Working MailSender

--- WpfMailSender/MainWindow.xaml.cs
+++ WpfMailSender/MainWindow.xaml.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using MailSender.Interfaces;
+using MailSender.Models;
+using MailSender.Services;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +24,59 @@ namespace WpfMailSender
     /// </summary>
     public partial class MainWindow : Window
     {
+        byte[] file;
+        string fileName;
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                IMailService service = new SmtpService();
+                var model = new MailModel
+                {
+                    From = tbFrom.Text,
+                    ToStr = tbTo.Text,
+                    Title = tbTitle.Text,
+                    Body = new TextRange(
+                        rtbBody.Document.ContentStart,
+                        rtbBody.Document.ContentEnd).Text,
+
+                };
+                model.Attachments.Add(new MailModel.Attachment {Name = FileName.Content.ToString() ,
+                    Content = file
+
+                });
+                MessageBox.Show(await service.Send(model));
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            var formDialog = new System.Windows.Forms.OpenFileDialog();
+            var result = formDialog.ShowDialog();
+            switch (result)
+            {
+                case System.Windows.Forms.DialogResult.OK:
+                    var fullFileName = formDialog.FileName;
+                    FileName.Content = fullFileName.Split('\\').LastOrDefault();
+                    file = File.ReadAllBytes(fullFileName);
+                    break;
+
+                case System.Windows.Forms.DialogResult.Cancel:
+                    default:
+                    FileName.Content = "...";
+                    break;
+            }
         }
     }
 }
